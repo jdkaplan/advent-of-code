49b3747f5c7b7dd6fcede4bdc52239392b1dabec
Jeremy Kaplan <jdkaplan@metagram.net>
2018-12-03 20:52:46 -0800

    Formatting

--- 2018/src/main.rs
+++ 2018/src/main.rs
@@ -1,9 +1,10 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
-    println!("{}", day1::part1());
-    println!("{}", day1::part2());
-    println!("{}", day2::part1());
-    println!("{}", day2::part2());
+    println!("Day 1 part 1: {}", day1::part1());
+    println!("Day 1 part 2: {}", day1::part2());
+    println!("Day 2 part 1: {}", day2::part1());
+    println!("Day 2 part 2: {}", day2::part2());
 }
